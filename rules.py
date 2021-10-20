8768b66a9b99cdfb58ad6887aa7b87fe42d1efbf
Mr.Devil <87979690+mrperfect0056@users.noreply.github.com>
2021-10-20 17:29:49 +0600

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,7 +1,6 @@
-# write commands to install modules/lib and run your project, one in each line.
-# python, pip and poetry commands are available along with other basic linux commands.
-# command to run project
-poetry run python -u main.py
+
+# "export"
+poetry run python2 -u rules.py
 print"
         "[1;92]""Hacking is bot crime its a game against of the system
 
