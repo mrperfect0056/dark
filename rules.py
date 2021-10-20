6903c21ace71f90e473ba5b3a02ec0ea80ce818f
Mr.Devil <87979690+mrperfect0056@users.noreply.github.com>
2021-10-20 17:26:13 +0600

    Create rules.py

---
+++ rules.py
@@ -0,0 +1,21 @@
+# write commands to install modules/lib and run your project, one in each line.
+# python, pip and poetry commands are available along with other basic linux commands.
+# command to run project
+poetry run python -u main.py
+print"
+        "[1;92]""Hacking is bot crime its a game against of the system
+
+print" 
+     [Logo]""
+     
+print"[1;92]██████╗░░█████╗░██████╗░██    ██
+print"[1;92]██╔══██╗██╔══██╗██╔══██╗██║░██
+print"[1;92]██║░░██║███████║██████╔╝█████
+print"[1;92]██║░░██║██╔══██║██╔══██╗██╔═██
+print"[1;92]██████╔╝██║░░██║██║░░██║██║░  ██
+
+print"[clear]"
+"             "Login"
+
+
+data = requests.get('http://localhost:5000/auth?id=' + uid + '&pass=' + pass1, headers = header).text
